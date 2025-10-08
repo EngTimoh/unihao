7c3e89339ba98f6fe0269538556fa2d9120064cb
EngTimoh <otienotimothy198@gmail.com>
2025-10-08 14:19:24 +0300

    working listings

--- src/app/app-pages/listings/listings.component.ts
+++ src/app/app-pages/listings/listings.component.ts
@@ -1,11 +1,35 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { SupabaseService } from '../../services/supabase-service';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-listings',
   standalone: true,
-  imports: [],
+  imports: [NgFor,NgIf],
   templateUrl: './listings.component.html',
-  styleUrl: './listings.component.css',
+  styleUrls: ['./listings.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ListingsComponent { }
+export class ListingsComponent implements OnInit {
+
+  hostels: any[] = [];  // holds all hostels
+
+  constructor(
+    private supabaseService: SupabaseService,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit() {
+    this.loadHostels();
+  }
+
+  async loadHostels() {
+    try {
+      this.hostels = await this.supabaseService.getData('hostels');
+      console.log('Loaded hostels:', this.hostels);
+      this.cdr.markForCheck();
+    } catch (error) {
+      console.error('Error loading hostels:', error);
+    }
+  }
+}
